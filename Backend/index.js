beaa846f590be2c555bc3a9157fca998e4a949ae
Abdelrahman Elsayed <108687433+d3cypherd@users.noreply.github.com>
2025-05-05 00:32:48 +0300

    add index.js

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 3000;
+
+async function start() {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
+
+  app.set("view engine", "ejs");
+  app.set("views", `${__dirname}/../Frontend/public/`);
+
+  app.use(morgan());
+
+  app.listen(PORT, () => {
+    console.log(`Backend listening on port ${PORT}`);
+  });
+}
+
+start();
